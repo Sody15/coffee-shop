b1e074d605e4c1f2b871a25f335a494e9a8439dd
Sody15 <pdsoderb@gmail.com>
2023-06-11 16:02:54 -0700

    changes

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'cof-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isTransparent = true;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset;
+    this.isTransparent = scrollPosition < 100;
+  }
+}
