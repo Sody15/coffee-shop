6ef29e0081737415183182d80ffaf7be0ef4ef82
Sody15 <pdsoderb@gmail.com>
2023-06-12 09:13:37 -0700

    added scroll animation

--- src/app/features/home/components/hero/hero.component.ts
+++ src/app/features/home/components/hero/hero.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'cof-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
+  animations: [
+    trigger('bounceAnimation', [
+      transition('* => bounce', [
+        animate(
+          '2s',
+          keyframes([
+            style({ transform: 'translateY(0)', offset: 0 }),
+            style({ transform: 'translateY(20px)', offset: 0.5 }),
+            style({ transform: 'translateY(0)', offset: 1 }),
+          ])
+        ),
+      ]),
+    ]),
+  ],
 })
-export class HeroComponent {
-
-}
+export class HeroComponent {}
--- src/app/shared/layout/two-col-container/two-col-container.component.ts
+++ src/app/shared/layout/two-col-container/two-col-container.component.ts
@@ -6,10 +6,10 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./two-col-container.component.scss'],
 })
 export class TwoColContainerComponent {
-  @Input() imgSrc: string = '';
+  @Input() imgSrc!: string;
   @Input() imgPosition: 'left' | 'right' = 'left';
 
-  @Input() subHeader: string = '';
-  @Input() header: string = '';
-  @Input() btnText: string = '';
+  @Input() subHeader!: string;
+  @Input() header!: string;
+  @Input() btnText!: string;
 }
