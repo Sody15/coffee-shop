0d8e77ea81e30b4be55c8424eda98f387caa7fae
Paul Soderberg <pdsoderb@gmail.com>
2023-06-13 10:23:51 -0700

    Added coffee bag images

--- src/app/core/data.ts
+++ src/app/core/data.ts
@@ -1,7 +1,12 @@
 import { Product } from './models/product';
 
 export const PRODUCTS: Product[] = [
-  { id: 1, name: 'Coffee 1', price: 23.99 },
-  { id: 2, name: 'Coffee 2', price: 21.99 },
-  { id: 3, name: 'Coffee 3', price: 19.99 },
+  { id: 1, name: "RoastMaster's Reserve", price: 23.99, imgSrc: 'coffee-bag-1.jpg' },
+  { id: 2, name: 'Bean Bliss', price: 21.99, imgSrc: 'coffee-bag-2.jpg' },
+  { id: 3, name: 'Caffeine Cascade', price: 19.99, imgSrc: 'coffee-bag-3.jpg' },
+  { id: 4, name: 'Mocha Marvel', price: 23.99, imgSrc: 'coffee-bag-4.jpg' },
+  { id: 5, name: 'Velvet Roast', price: 21.99, imgSrc: 'coffee-bag-5.jpg' },
+  { id: 6, name: 'Java Jive', price: 19.99, imgSrc: 'coffee-bag-6.jpg' },
+  { id: 7, name: 'Espresso Euphoria', price: 23.99, imgSrc: 'coffee-bag-7.jpg' },
+  { id: 8, name: 'Flavorful Fusion', price: 21.99, imgSrc: 'coffee-bag-8.jpg' },
 ];
