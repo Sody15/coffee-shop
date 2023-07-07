515016bdebb4405fd2cc5b28df6a86709468fb4b
Paul Soderberg <pdsoderb@gmail.com>
2023-07-07 13:45:54 -0700

    refactored show hide of header/footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Store, select } from '@ngrx/store';
+import { Observable, map } from 'rxjs';
+
+import { selectUrl } from 'src/app/routing.selectors';
+import { RouterState } from '@ngrx/router-store';
+
+const hideHeaderFooterRoutes = ['/checkout'];
+
 @Component({
   selector: 'cof-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent {
+  // Hide/show header based on route
+  hideHeaderFooter$: Observable<boolean> = this.store.pipe(
+    select(selectUrl),
+    map((url) => hideHeaderFooterRoutes.includes(url))
+  );
+
+  constructor(private store: Store<{ router: RouterState }>) {}
+}
--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -9,39 +9,26 @@ import { selectCartNumItems } from '@app-core/state/cart.selectors';
 import { selectUrl } from 'src/app/routing.selectors';
 import { RouterState } from '@ngrx/router-store';
 
-const noHeaderRoutes = ['/checkout'];
-
 @Component({
   selector: 'cof-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  isHidden = false;
   isTransparent = true;
   showCart = false;
   isMenuOpen = false;
 
-  // Hide/show header based on route
-  routerState$: Observable<any> = this.store.pipe(
-    select(selectUrl),
-    tap((url) => {
-      this.isHidden = noHeaderRoutes.includes(url);
-    })
-  );
-
   // Set number of products in cart
   cartNumProducts$: Observable<number> = this.store.select(selectCartNumItems);
 
-  constructor(private store: Store<{ router: RouterState; cart: CartState }>) {}
+  constructor(private store: Store<{ cart: CartState }>) {}
 
   // Toggle header transparency based on scroll pos
   @HostListener('window:scroll', ['$event'])
   onWindowScroll() {
-    if (!this.isHidden) {
-      const scrollPosition = window.scrollY;
-      this.isTransparent = scrollPosition < 30;
-    }
+    const scrollPosition = window.scrollY;
+    this.isTransparent = scrollPosition < 30;
   }
 
   // Toggle cart
